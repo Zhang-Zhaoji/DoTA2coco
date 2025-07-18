5a94b81a6023a0f053b8794c059247c8f24bb5dd
Zhaoji Zhang <121226227+Zhang-Zhaoji@users.noreply.github.com>
2025-07-18 19:22:51 +0800

    Add files via upload

--- process_src/coco_arrangement.py
+++ process_src/coco_arrangement.py
@@ -3,8 +3,14 @@ import os
 import json
 import tqdm
 import pathlib
+import numpy as np
 
-def generate_template_image_dict(image_id, width, height, file_name):
+randomly_sample = 50 # 1 in {sample} as test
+
+_global_image_id_counter = 0
+_global_annotation_id_counter = 0
+
+def generate_template_image_dict(image_id:int, width:int, height:int, file_name:str):
     """
     implement further processing codes later
     """
@@ -15,7 +21,7 @@ def generate_template_image_dict(image_id, width, height, file_name):
     "file_name": file_name
     }
 
-def generate_template_annotation_dict(id, image_id, category_id, segmentation, area, bbox, iscrowd):
+def generate_template_annotation_dict(id:int, image_id:int, category_id:int, segmentation, area, bbox, iscrowd):
     """
     implement further processing codes later
     """
@@ -73,8 +79,8 @@ def generate_coco_style_json():
     tgt_json['annotations'] = []
     tgt_json['categories'] = []
     return tgt_json
-def copy_and_modify(annotation_file, annotation_root, image_root, folders, new_root):
-    
+def copy_and_modify(annotation_file, annotation_root, image_root, folders, new_root, image_id_map):
+    global _global_image_id_counter, _global_annotation_id_counter # Ensure these are accessible
     # Images used to be stored in the folders representing videos.
     # We now store them in the same but another root directory.
     
@@ -92,8 +98,23 @@ def copy_and_modify(annotation_file, annotation_root, image_root, folders, new_r
         image_names = [os.path.join(new_root,f'{annotation_folder}_{image_name}') for image_name in images]
         # load all folder names, generate corresponding new_img names and original_img_paths
         # open original annotation file and modify annotation file
+        
         video_information = json.load(open(os.path.join(annotation_root,annotation_folder+'.json'),'r'))
+        original_img_name_to_label_idx = {}
+
+        for i, label_entry in enumerate(video_information['labels']):
+            # Assuming label_entry['image_path'] is something like "frames/video_id/images/000052.jpg"
+            # We need to extract just "000052.jpg"
+            original_img_filename_from_label = os.path.basename(label_entry['image_path'])
+            original_img_name_to_label_idx[original_img_filename_from_label] = i
+        
         for idx, img_name in enumerate(images[:min(len(images),len(video_information['labels']))]):
+            if np.random.uniform(0,1,1) > 1/randomly_sample:
+                continue
+            # Increment global image ID counter and use it
+            current_image_id = _global_image_id_counter
+            _global_image_id_counter += 1
+
             img_id = int(img_name.split('.')[0]) - 1 # for generated saliency map
             # print(img_id, len(images), len(video_information['labels']), video_information['num_frames'],annotation_folder)
             img_annotation = video_information['labels'][img_id]
@@ -120,16 +141,23 @@ def copy_and_modify(annotation_file, annotation_root, image_root, folders, new_r
             # },
             pre_img_path = os.path.join(image_root, folder, img_name)
             new_img_path = image_names[idx]
-            image_dict = generate_template_image_dict(f'{annotation_folder}_{img_id:06d}',1280,720,new_img_path)
+
+            image_id_map[str(current_image_id)] = {'original_folder': folder, 'original_filename': img_name, 'new_path': new_img_path}
+
+            image_dict = generate_template_image_dict(current_image_id,1280,720,new_img_path)
             annotation_file['images'].append(image_dict)
             shutil.copy(pre_img_path, new_img_path)
             objects = img_annotation['objects']
             for obj_idx, object in enumerate(objects):
+                # Increment global annotation ID counter and use it
+                current_annotation_id = _global_annotation_id_counter
+                _global_annotation_id_counter += 1
+
                 bbox = object['bbox']
                 bbox = [bbox[0],bbox[1],-bbox[0]+bbox[2],-bbox[1]+bbox[3]]
                 category_id = object["category ID"]
-                annotation_dict = generate_template_annotation_dict(id = f'{annotation_folder}_{img_id:06d}_{obj_idx}',
-                                                                image_id = f'{annotation_folder}_{img_id:06d}',
+                annotation_dict = generate_template_annotation_dict(id = current_annotation_id,#f'{annotation_folder}_{img_id:06d}_{obj_idx}',
+                                                                image_id = current_image_id,# f'{annotation_folder}_{img_id:06d}',
                                                                 category_id=category_id,
                                                                 segmentation=[bbox],
                                                                 area=bbox[2]*bbox[3],
@@ -144,6 +172,12 @@ def save_annotation_Files(annotation_file, output_file):
         print('Saved:', output_file)
             
 def prepare_dataset(_root = './', dtfolder_name = 'DoTA', anno_dir = './processedAnnotations'):
+    global _global_image_id_counter, _global_annotation_id_counter # Access global counters
+    
+    # Reset counters for each dataset preparation run to ensure unique IDs across runs
+    _global_image_id_counter = 0
+    _global_annotation_id_counter = 0
+    
     # load splits ids
     print('Loading splits...')
     train_split_path = os.path.join(anno_dir, 'train_split.txt')
@@ -170,6 +204,9 @@ def prepare_dataset(_root = './', dtfolder_name = 'DoTA', anno_dir = './processe
     val_annotation = generate_coco_style_json()
     val_annotation['categories'] = generate_dota_categories()
 
+    train_image_id_map = {}
+    val_image_id_map = {}
+
     # move images, first trains
     # txt file contain the folder names of each image, we need to move them into the corresponding folder
     # perhaps we should write a new function to do this
@@ -179,18 +216,25 @@ def prepare_dataset(_root = './', dtfolder_name = 'DoTA', anno_dir = './processe
                     annotation_root=os.path.join(anno_dir,'annotations'), 
                     image_root='saliency_imgs', 
                     folders=[os.path.join(train_split_txt[i]) for i in range(len(train_split_txt))],
-                    new_root=train_dataset_root)
+                    new_root=train_dataset_root,
+                    image_id_map = train_image_id_map)
     # valid
     print("Copying and modifying annotations for valid set")
     copy_and_modify(annotation_file=val_annotation, 
                     annotation_root=os.path.join(anno_dir,'annotations'), 
                     image_root='saliency_imgs', 
                     folders=[os.path.join(val_split_txt[i]) for i in range(len(val_split_txt))],
-                    new_root=val_dataset_root)
+                    new_root=val_dataset_root,
+                    image_id_map = val_image_id_map)
     # save annotation_files
     print("Save Annotations")
     save_annotation_Files(train_annotation, os.path.join(annotation_folder, 'train.json'))
     save_annotation_Files(val_annotation, os.path.join(annotation_folder, 'val.json'))
+    print("Done")
+
+    print("Saving Image ID Maps...")
+    save_annotation_Files(train_image_id_map, os.path.join(annotation_folder, 'train_image_id_map.json'))
+    save_annotation_Files(val_image_id_map, os.path.join(annotation_folder, 'val_image_id_map.json'))
     print("Done")
 
 def main():
